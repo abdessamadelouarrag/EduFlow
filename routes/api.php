49e0ced60a04ef5e53181842ee703cbf9269d627
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-03-24 16:03:09 +0100

    update file of env fo config auth with JWT

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
